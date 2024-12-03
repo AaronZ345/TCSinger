9f1e5eadc257632fe6a6c4b0eabd666de1118dac
Yu Zhang <34849476+AaronZ345@users.noreply.github.com>
2024-12-03 13:40:23 +0800

    Update style_adapt_decoder.py

--- modules/TCSinger/style_adapt_decoder.py
+++ modules/TCSinger/style_adapt_decoder.py
@@ -1,5 +1,7 @@
+from collections import deque
 from functools import partial
-from modules.TCSinger.diff.shallow_diffusion import *
+from inspect import isfunction
+from pathlib import Path
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -8,10 +10,60 @@ from tqdm import tqdm
 from utils.commons.hparams import hparams
 
 
-# SAD
-class SADecoder(nn.Module):
-    def __init__(self, out_dims, denoise_fn,
-                 timesteps=1000, time_scale=1, loss_type='l1', betas=None, spec_min=None, spec_max=None):
+def exists(x):
+    return x is not None
+
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+
+# gaussian diffusion trainer class
+
+def extract(a, t, x_shape):
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
+
+
+def noise_like(shape, device, repeat=False):
+    repeat_noise = lambda: torch.randn((1, *shape[1:]), device=device).repeat(shape[0], *((1,) * (len(shape) - 1)))
+    noise = lambda: torch.randn(shape, device=device)
+    return repeat_noise() if repeat else noise()
+
+
+def linear_beta_schedule(timesteps, max_beta=hparams.get('max_beta', 0.01)):
+    """
+    linear schedule
+    """
+    betas = np.linspace(1e-4, max_beta, timesteps)
+    return betas
+
+
+def cosine_beta_schedule(timesteps, s=0.008):
+    """
+    cosine schedule
+    as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
+    """
+    steps = timesteps + 1
+    x = np.linspace(0, steps, steps)
+    alphas_cumprod = np.cos(((x / steps) + s) / (1 + s) * np.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return np.clip(betas, a_min=0, a_max=0.999)
+
+
+beta_schedule = {
+    "cosine": cosine_beta_schedule,
+    "linear": linear_beta_schedule,
+}
+
+
+class GaussianDiffusion(nn.Module):
+    def __init__(self, phone_encoder, out_dims, denoise_fn,
+                 timesteps=1000, K_step=1000, loss_type=hparams.get('diff_loss_type', 'l1'), betas=None, spec_min=None, spec_max=None):
         super().__init__()
         self.denoise_fn = denoise_fn
         self.mel_bins = out_dims
@@ -19,29 +71,27 @@ class SADecoder(nn.Module):
         if exists(betas):
             betas = betas.detach().cpu().numpy() if isinstance(betas, torch.Tensor) else betas
         else:
-            betas = get_noise_schedule_list(
-                schedule_mode=hparams['schedule_type'],
-                timesteps=timesteps + 1,
-                min_beta=0.1,
-                max_beta=40,
-                s=0.008,
-            )
+            if 'post_schedule_type' in hparams.keys():
+                betas = beta_schedule[hparams['post_schedule_type']](timesteps)
+            else:
+                betas = cosine_beta_schedule(timesteps)
 
         alphas = 1. - betas
         alphas_cumprod = np.cumprod(alphas, axis=0)
         alphas_cumprod_prev = np.append(1., alphas_cumprod[:-1])
 
-        self.time_scale = time_scale
+        timesteps, = betas.shape
         self.num_timesteps = int(timesteps)
+        self.K_step = K_step
         self.loss_type = loss_type
+
+        self.noise_list = deque(maxlen=4)
 
         to_torch = partial(torch.tensor, dtype=torch.float32)
 
-        self.register_buffer('timesteps', to_torch(self.num_timesteps))      # beta
-        self.register_buffer('timescale', to_torch(self.time_scale))      # beta
-        self.register_buffer('betas', to_torch(betas))      # beta
-        self.register_buffer('alphas_cumprod', to_torch(alphas_cumprod)) # alphacum_t
-        self.register_buffer('alphas_cumprod_prev', to_torch(alphas_cumprod_prev)) # alphacum_{t-1}
+        self.register_buffer('betas', to_torch(betas))
+        self.register_buffer('alphas_cumprod', to_torch(alphas_cumprod))
+        self.register_buffer('alphas_cumprod_prev', to_torch(alphas_cumprod_prev))
 
         # calculations for diffusion q(x_t | x_{t-1}) and others
         self.register_buffer('sqrt_alphas_cumprod', to_torch(np.sqrt(alphas_cumprod)))
@@ -85,98 +135,163 @@ class SADecoder(nn.Module):
         posterior_log_variance_clipped = extract(self.posterior_log_variance_clipped, t, x_t.shape)
         return posterior_mean, posterior_variance, posterior_log_variance_clipped
 
-    def q_posterior_sample(self, x_start, x_t, t, repeat_noise=False):
-        b, *_, device = *x_start.shape, x_start.device
-        model_mean, _, model_log_variance = self.q_posterior(x_start=x_start, x_t=x_t, t=t)
-        noise = noise_like(x_start.shape, device, repeat_noise)
+    def p_mean_variance(self, x, t, cond, style, clip_denoised: bool):
+        noise_pred = self.denoise_fn(x, t, cond=cond, style=style)
+        x_recon = self.predict_start_from_noise(x, t=t, noise=noise_pred)
+
+        if clip_denoised:
+            x_recon.clamp_(-1., 1.)
+
+        model_mean, posterior_variance, posterior_log_variance = self.q_posterior(x_start=x_recon, x_t=x, t=t)
+        return model_mean, posterior_variance, posterior_log_variance
+
+    @torch.no_grad()
+    def p_sample(self, x, t, cond, style, clip_denoised=True, repeat_noise=False):
+        b, *_, device = *x.shape, x.device
+        model_mean, _, model_log_variance = self.p_mean_variance(x=x, t=t, cond=cond, style=style, clip_denoised=clip_denoised)
+        noise = noise_like(x.shape, device, repeat_noise)
         # no noise when t == 0
-        nonzero_mask = (1 - (t == 0).float()).reshape(b, *((1,) * (len(x_start.shape) - 1)))
+        nonzero_mask = (1 - (t == 0).float()).reshape(b, *((1,) * (len(x.shape) - 1)))
         return model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
 
     @torch.no_grad()
-    def p_sample(self, x_t, t, cond, style,spk_emb=None, clip_denoised=True, repeat_noise=False):
-        b, *_, device = *x_t.shape, x_t.device
-        x_0_pred = self.denoise_fn(x_t, t, cond,style)
+    def p_sample_plms(self, x, t, interval, cond, clip_denoised=True, repeat_noise=False):
+        """
+        Use the PLMS method from [Pseudo Numerical Methods for Diffusion Models on Manifolds](https://arxiv.org/abs/2202.09778).
+        """
 
-        return self.q_posterior_sample(x_start=x_0_pred, x_t=x_t, t=t)
+        def get_x_pred(x, noise_t, t):
+            a_t = extract(self.alphas_cumprod, t, x.shape)
+            a_prev = extract(self.alphas_cumprod, torch.max(t-interval, torch.zeros_like(t)), x.shape)
+            a_t_sq, a_prev_sq = a_t.sqrt(), a_prev.sqrt()
 
-    @torch.no_grad()
-    def interpolate(self, x1, x2, t, cond, spk_emb, lam=0.5):
-        b, *_, device = *x1.shape, x1.device
-        t = default(t, self.num_timesteps - 1)
+            x_delta = (a_prev - a_t) * ((1 / (a_t_sq * (a_t_sq + a_prev_sq))) * x - 1 / (a_t_sq * (((1 - a_prev) * a_t).sqrt() + ((1 - a_t) * a_prev).sqrt())) * noise_t)
+            x_pred = x + x_delta
 
-        assert x1.shape == x2.shape
+            return x_pred
 
-        t_batched = torch.stack([torch.tensor(t, device=device)] * b)
-        xt1, xt2 = map(lambda x: self.q_sample(x, t=t_batched), (x1, x2))
+        noise_list = self.noise_list
+        noise_pred = self.denoise_fn(x, t, cond=cond)
 
-        x = (1 - lam) * xt1 + lam * xt2
-        for i in tqdm(reversed(range(0, t)), desc="interpolation sample time step", total=t):
-            x = self.p_sample(x, torch.full((b,), i, device=device, dtype=torch.long), cond, spk_emb)
-        x = x[:, 0].transpose(1, 2)
-        return self.denorm_spec(x)
+        if len(noise_list) == 0:
+            x_pred = get_x_pred(x, noise_pred, t)
+            noise_pred_prev = self.denoise_fn(x_pred, max(t-interval, 0), cond=cond)
+            noise_pred_prime = (noise_pred + noise_pred_prev) / 2
+        elif len(noise_list) == 1:
+            noise_pred_prime = (3 * noise_pred - noise_list[-1]) / 2
+        elif len(noise_list) == 2:
+            noise_pred_prime = (23 * noise_pred - 16 * noise_list[-1] + 5 * noise_list[-2]) / 12
+        elif len(noise_list) >= 3:
+            noise_pred_prime = (55 * noise_pred - 59 * noise_list[-1] + 37 * noise_list[-2] - 9 * noise_list[-3]) / 24
+
+        x_prev = get_x_pred(x, noise_pred_prime, t)
+        noise_list.append(noise_pred)
+
+        return x_prev
 
     def q_sample(self, x_start, t, noise=None):
         noise = default(noise, lambda: torch.randn_like(x_start))
-
         return (
                 extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start +
                 extract(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape) * noise
         )
 
-    def diffuse_trace(self, x_start, mask):
-        b, *_, device = *x_start.shape, x_start.device
-        trace = [self.norm_spec(x_start).clamp_(-1., 1.) * ~mask.unsqueeze(-1)]
-        for t in range(self.num_timesteps):
-            t = torch.full((b,), t, device=device, dtype=torch.long)
-            trace.append(
-                self.diffuse_fn(x_start, t)[:, 0].transpose(1, 2) * ~mask.unsqueeze(-1)
-            )
-        return trace
-
-    def diffuse_fn(self, x_start, t, noise=None):
-        x_start = self.norm_spec(x_start)
-        x_start = x_start.transpose(1, 2)[:, None, :, :]  # [B, 1, M, T]
-        zero_idx = t < 0 # for items where t is -1
-        t[zero_idx] = 0
+    def p_losses(self, x_start, t, cond, style,noise=None, nonpadding=None):
         noise = default(noise, lambda: torch.randn_like(x_start))
-        out = self.q_sample(x_start=x_start, t=t, noise=noise)
-        out[zero_idx] = x_start[zero_idx] # set x_{-1} as the gt mel
-        return out
 
-    def forward(self, txt_tokens, ret,
-                ref_mels=None, infer=False):
-        b, *_, device = *txt_tokens.shape, txt_tokens.device
-        nonpadding = (ret['mel2ph'] != 0).float().unsqueeze(1).unsqueeze(1) # [B, T]
-        cond = ret['decoder_inp'].transpose(1, 2)
-        style=ret['style']
-        if not infer:
-            t = torch.randint(0, self.num_timesteps + 1, (b,), device=device).long()
-            # Diffusion
-            x_t = self.diffuse_fn(ref_mels, t) * nonpadding
+        x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
+        x_recon = self.denoise_fn(x_noisy, t, cond,style)
 
-            # Predict x_{start}
-            x_0_pred = self.denoise_fn(x_t, t, cond,style) * nonpadding
+        if self.loss_type == 'l1':
+            if nonpadding is not None:
+                loss = ((noise - x_recon).abs() * nonpadding.unsqueeze(1)).mean()
+            else:
+                # print('are you sure w/o nonpadding?')
+                loss = (noise - x_recon).abs().mean()
 
-            ret['mel_out'] = x_0_pred[:, 0].transpose(1, 2) # [B, T, 80]
+        elif self.loss_type == 'l2':
+            loss = F.mse_loss(noise, x_recon)
         else:
-            t = self.num_timesteps  # reverse总步数
-            shape = (cond.shape[0], 1, self.mel_bins, cond.shape[2])
-            x = torch.randn(shape, device=device)  # noise
-            for i in tqdm(reversed(range(0, t)), desc='ProDiff Teacher sample time step', total=t):
-                x = self.p_sample(x, torch.full((b,), i,device=device, dtype=torch.long), cond,style=style)  # x(mel), t, condition(phoneme)
+            raise NotImplementedError()
+
+        return loss
+
+    def forward(self, txt_tokens, ph2word, word_len, mel2word=None, mel2ph=None, spk_embed=None, infer_spk_embed=None, f0=None,
+                uv=None, tgt_mels=None, infer=False, note_tokens = None, note_durs = None, note_types = None, note2words = None, mel2notes=None
+                , **kwargs):
+        b, *_, device = *txt_tokens.shape, txt_tokens.device
+        ret = self.fs2(txt_tokens, ph2word, word_len, mel2word, mel2ph, spk_embed,  infer_spk_embed, f0, uv, tgt_mels, infer, note_tokens, note_durs, note_types, note2words, mel2notes, skip_decoder=False, **kwargs)
+        cond = ret['decoder_inp'].transpose(1, 2)
+
+        if not infer:
+            t = torch.randint(0, self.K_step, (b,), device=device).long()
+            x = tgt_mels
+            x = self.norm_spec(x)
+            x = x.transpose(1, 2)[:, None, :, :]  # [B, 1, M, T]
+            ret['diff_loss'] = self.p_losses(x, t, cond)
+
+        else:
+            ret['fs2_mel'] = ret['mel_out']
+            fs2_mels = ret['mel_out']
+            t = self.K_step
+            fs2_mels = self.norm_spec(fs2_mels)
+            fs2_mels = fs2_mels.transpose(1, 2)[:, None, :, :]
+
+            x = self.q_sample(x_start=fs2_mels, t=torch.tensor([t - 1], device=device).long())
+
+            if hparams.get('pndm_speedup'):
+                self.noise_list = deque(maxlen=4)
+                iteration_interval = hparams['pndm_speedup']
+                for i in tqdm(reversed(range(0, t, iteration_interval)), desc='sample time step',
+                              total=t // iteration_interval):
+                    x = self.p_sample_plms(x, torch.full((b,), i, device=device, dtype=torch.long), iteration_interval,
+                                           cond)
+            else:
+                for i in tqdm(reversed(range(0, t)), desc='sample time step', total=t):
+                    x = self.p_sample(x, torch.full((b,), i, device=device, dtype=torch.long), cond)
             x = x[:, 0].transpose(1, 2)
-            ret['mel_out'] = self.denorm_spec(x)  # 去除norm
+            if mel2ph is not None:  # for singing
+                ret['mel_out'] = self.denorm_spec(x) * ((mel2ph > 0).float()[:, :, None])
+            else:
+                ret['mel_out'] = self.denorm_spec(x)
         return ret
 
     def norm_spec(self, x):
-        return x
+        return (x - self.spec_min) / (self.spec_max - self.spec_min) * 2 - 1
 
     def denorm_spec(self, x):
-        return x
+        return (x + 1) / 2 * (self.spec_max - self.spec_min) + self.spec_min
 
     def cwt2f0_norm(self, cwt_spec, mean, std, mel2ph):
         return self.fs2.cwt2f0_norm(cwt_spec, mean, std, mel2ph)
 
     def out2mel(self, x):
         return x
+
+
+# SAP
+class StyleDecoder(GaussianDiffusion):
+    def forward(self, cond, ref_mels, coarse_mels, ret, infer):
+        b, *_, device = *cond.shape, cond.device
+
+        style=ret['style']
+        cond = cond.transpose(1, 2)
+        fs2_mels = coarse_mels
+
+        if not infer:
+            t = torch.randint(0, self.K_step, (b,), device=device).long()
+            x = self.norm_spec(ref_mels)
+            x = x.transpose(1, 2)[:, None, :, :]  # [B, 1, M, T]
+            ret['diff'] = self.p_losses(x, t, cond,style)
+        else:
+            t = self.K_step
+            fs2_mels = self.norm_spec(fs2_mels)
+            fs2_mels = fs2_mels.transpose(1, 2)[:, None, :, :]
+
+            x = self.q_sample(x_start=fs2_mels, t=torch.tensor([t - 1], device=device).long())
+
+            for i in tqdm(reversed(range(0, t)), desc='sample time step', total=t):
+                x = self.p_sample(x, torch.full((b,), i, device=device, dtype=torch.long), cond,style)
+            x = x[:, 0].transpose(1, 2)
+            ret['mel_out'] = self.denorm_spec(x)
+            ret['diff'] = 0.0
